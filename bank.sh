a4b42ebecdbc12576c1d3e735ec8f9af4a086cfe
BodjeBodje <bouriahdiallo93@gmail.com>
2026-09-26 12:27:44 -0400

    we worked to logic: deposit, withdraw, and check balance

--- bank.sh
+++ bank.sh
@@ -11,13 +11,30 @@ while true; do
     read -p  "Choose an option: " choose
     case $choose in
         1)
-            echo "Deposit section"
+            read -p "Enter amount deposit: " amount #500
+                if [[ $amount -ge 0 ]]; then
+                    deposit=$((init_balance+amount)) #1000+500
+                    echo "$deposit"
+                else
+                    echo "Invalid amount "
+                fi
+                #if [[ "$amount" =~ ^[0-9]+$ ]]; then
+            # echo "$deposit" #1500
+            # echo "Deposit section"
             ;;
         2)
+            read -p "Enter your Withdraw amount " withd
+            if [[ deposit -gt $withd ]]; then
+                deposit=$((deposit-withd))
+                echo "Sufficient amount $deposit"
+            else
+                echo "Unfficient amount from balance $deposit"
+                
+            fi
            echo "Withdraw section"
            ;;
         3)
-            echo "Your current balance: $init_balance"
+            echo "Your current balance: $deposit"
             ;;
         4)
             echo "Goodbye!"
